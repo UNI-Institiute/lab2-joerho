d2f5fe225f3c16dd09b9774da1e602a112db34f5
joerho1@gmail.com <joerho1@gmail.com>
2020-08-23 11:42:49 -0700

    uploading

---
+++ funcs/funcs_tests.py
@@ -0,0 +1,16 @@
+import unittest
+import funcs
+
+class TestCases(unittest.TestCase):
+    def test_f_1(self):
+    	pass
+
+
+    def test_f_2(self):
+    	pass
+        
+
+# Run the unit tests.
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ funcs_objects/funcs_objects_tests.py
@@ -0,0 +1,18 @@
+import unittest
+import objects
+import funcs_objects
+
+class TestCases(unittest.TestCase):
+    def test_point(self):
+        pass
+
+
+    def test_circle(self):
+        pass
+
+    # Add other testing functions
+
+# Run the unit tests.
+if __name__ == '__main__':
+    unittest.main()
+
